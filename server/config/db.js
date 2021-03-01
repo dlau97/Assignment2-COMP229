8999d8cc4c5c1b34fd83b715f692f0d339899719
Dhevan Lau <dhevan999@gmail.com>
2021-03-01 22:43:00 +0200

    switched from local to online DB

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI":"mongodb://localhost/contacts"
+    //"URI":"mongodb://localhost/contacts"
+    "URI":"mongodb+srv://dhevan:Toolaud572@mongodbserver.oicwq.mongodb.net/business_contacts?retryWrites=true&w=majority"
 }
