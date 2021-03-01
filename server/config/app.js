aba80177528337a68c14bad36d966ec2f35c34c9
Dhevan Lau <dhevan999@gmail.com>
2021-03-01 17:35:45 +0200

    imported authentication packages

--- server/config/app.js
+++ server/config/app.js
@@ -4,6 +4,13 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+//modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportLocal= require('passport-local');
+let localStrategy = passportLocal.Strategy;
+let flash = require('connect-flash');
+
 // database setup
 let mongoose = require('mongoose');
 let DB = require('./db');
@@ -33,6 +40,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../node_modules')));
+
+//setup express session
+app.use(session({
+  secret: "SomeSecret",
+  saveUninitialized: false,
+  resave:false
+}));
+
+// initialise flash
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
